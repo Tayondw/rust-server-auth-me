a4873b48ff713f2abf6d48412d1ab9d67a4ec628
Tayon <tayondw@gmail.com>
2025-04-07 15:14:55 -0400

    removed db.rs because we are connecting to the database in the main file at the let manager declaration

--- auth-me/src/db.rs
+++
@@ -1,8 +0,0 @@
-use diesel::prelude::*;
-
-// established a connection to the database
-// if connection fails, panic
-pub fn establish_connection(database_url: &str) -> PgConnection {
-    PgConnection::establish(database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
