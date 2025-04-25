f0072537c10804767a121c1a99c7028b4743cb8b
Tayon <tayondw@gmail.com>
2025-04-25 19:34:14 -0400

    corrected post handlers to better reflect the usage of custom error messages in the errors.rs file

--- auth-me/src/routes/api/posts.rs
+++ auth-me/src/routes/api/posts.rs
@@ -6,18 +6,23 @@ use axum::{
     Json,
     http::StatusCode,
 };
-use diesel::prelude::*;
+use diesel::{
+    prelude::*,
+    result::Error,
+    r2d2::{ ConnectionManager, PooledConnection },
+    PgConnection,
+};
 
 use crate::{
     models::Post,
     schema::posts::{ self },
     AppState,
-    ErrorResponse,
     database::{
         operations::posts::{ get_posts_by_user, create_post, update_post, delete_post },
         DbConnExt,
     },
     routes::api::{ CreatePostRequest, UpdatePostRequest, PostQuery },
+    errors::{ HttpError, ErrorMessage },
 };
 
 // POST ROUTER
@@ -32,28 +37,17 @@ pub fn post_routes() -> Router<Arc<AppState>> {
 }
 
 // GET ALL POSTS
-pub async fn get_posts(State(state): State<Arc<AppState>>) -> Result<
-    Json<Vec<Post>>,
-    (StatusCode, Json<ErrorResponse>)
-> {
-    let mut conn = state.conn()?;
+pub async fn get_posts(State(state): State<Arc<AppState>>) -> Result<Json<Vec<Post>>, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
     // Execute the query (directly, no interact needed)
-    let posts_result: Result<Vec<Post>, (StatusCode, Json<ErrorResponse>)> = posts::table
+    let posts_result: Result<Vec<Post>, Error> = posts::table
         .select(Post::as_select())
-        .load(&mut *conn)
-        .map_err(|e: diesel::result::Error| {
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ErrorResponse {
-                    message: format!("Database error: {}", e),
-                }),
-            )
-        });
+        .load(&mut *conn);
 
     match posts_result {
         Ok(posts) => Ok(Json(posts)),
-        Err(e) => Err(e),
+        Err(_) => Err(HttpError::server_error(ErrorMessage::DatabaseError.to_string())),
     }
 }
 
@@ -61,37 +55,23 @@ pub async fn get_posts(State(state): State<Arc<AppState>>) -> Result<
 pub async fn get_post_by_id(
     State(state): State<Arc<AppState>>,
     Path(post_id): Path<i32>
-) -> Result<Json<Post>, (StatusCode, Json<ErrorResponse>)> {
-    let mut conn = state.conn()?;
+) -> Result<Json<Post>, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
-    // Query the database for the user
+    // Query the database for the post
     let post_result = posts::table
         .find(post_id) // Using find for primary key lookup
         .select(Post::as_select())
         .first(&mut *conn)
         .map_err(|e| {
             match e {
-                diesel::result::Error::NotFound =>
-                    (
-                        StatusCode::NOT_FOUND,
-                        Json(ErrorResponse {
-                            message: format!("Post with id {} not found", post_id),
-                        }),
-                    ),
-                _ =>
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(ErrorResponse {
-                            message: format!("Database error: {}", e),
-                        }),
-                    ),
+                Error::NotFound =>
+                    HttpError::new(ErrorMessage::PostNotFound.to_string(), StatusCode::NOT_FOUND),
+                _ => HttpError::server_error(ErrorMessage::DatabaseError.to_string()),
             }
-        });
+        })?;
 
-    match post_result {
-        Ok(post) => Ok(Json(post)),
-        Err(e) => Err(e),
-    }
+    Ok(Json(post_result))
 }
 
 // GET POSTS BY USER
@@ -99,40 +79,28 @@ pub async fn get_posts_by_user_handler(
     State(state): State<Arc<AppState>>,
     Path(user): Path<i32>,
     Query(query): Query<PostQuery>
-) -> Result<Json<Vec<Post>>, (StatusCode, Json<ErrorResponse>)> {
-    let mut conn = state.conn()?;
+) -> Result<Json<Vec<Post>>, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
     let limit = query.limit.unwrap_or(10);
     let offset = query.offset.unwrap_or(0);
     let sort_order = query.sort.clone(); // asc or desc
 
-    match get_posts_by_user(&mut conn, user, limit, offset, sort_order) {
-        Ok(posts) => Ok(Json(posts)),
-        Err(e) =>
-            Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ErrorResponse {
-                    message: format!("Error fetching posts for user {}: {}", user, e),
-                }),
-            )),
-    }
+    get_posts_by_user(&mut conn, user, limit, offset, sort_order)
+        .map(Json)
+        .map_err(|_| { HttpError::server_error(ErrorMessage::PostsByUserError.to_string()) })
 }
 
 // CREATE NEW POST
 pub async fn create_post_handler(
     State(state): State<Arc<AppState>>,
     Json(post_data): Json<CreatePostRequest>
-) -> Result<Json<Post>, (StatusCode, Json<ErrorResponse>)> {
-    let mut conn = state.conn()?;
+) -> Result<Json<Post>, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
     create_post(&mut conn, post_data.title, post_data.content, post_data.user_id)
-        .map_err(|e| (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(ErrorResponse {
-                message: format!("Failed to create post: {}", e),
-            }),
-        ))
         .map(Json)
+        .map_err(|_| HttpError::server_error(ErrorMessage::PostCreationError.to_string()))
 }
 
 // UPDATE POST BY ID
@@ -140,43 +108,24 @@ pub async fn update_post_handler(
     State(state): State<Arc<AppState>>,
     Path(post_id): Path<i32>,
     Json(update_data): Json<UpdatePostRequest>
-) -> Result<Json<Post>, (StatusCode, Json<ErrorResponse>)> {
-    let mut conn = state.conn()?;
+) -> Result<Json<Post>, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
     update_post(&mut conn, post_id, update_data.title, update_data.content)
-        .map_err(|e| {
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ErrorResponse {
-                    message: format!("Failed to update post: {}", e),
-                }),
-            )
-        })
         .map(Json)
+        .map_err(|_| HttpError::server_error(ErrorMessage::PostUpdateError.to_string()))
 }
 
 // DELETE POST BY ID
 pub async fn delete_post_handler(
     State(state): State<Arc<AppState>>,
     Path(post_id): Path<i32>
-) -> Result<StatusCode, (StatusCode, Json<ErrorResponse>)> {
-    let mut conn = state.conn()?;
+) -> Result<StatusCode, HttpError> {
+    let mut conn: PooledConnection<ConnectionManager<PgConnection>> = state.conn()?;
 
     match delete_post(&mut conn, post_id).await {
         Ok(_) => Ok(StatusCode::NO_CONTENT),
-        Err(diesel::result::Error::NotFound) =>
-            Err((
-                StatusCode::NOT_FOUND,
-                Json(ErrorResponse {
-                    message: format!("Post with id {} not found", post_id),
-                }),
-            )),
-        Err(e) =>
-            Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ErrorResponse {
-                    message: format!("Failed to delete post: {}", e),
-                }),
-            )),
+        Err(Error::NotFound) => Err(HttpError::not_found(ErrorMessage::PostNotFound.to_string())),
+        Err(_) => Err(HttpError::server_error(ErrorMessage::DeletePostError.to_string())),
     }
 }
