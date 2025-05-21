48e80cae3a4dcecce05cd2927ce1711dc1f5c86e
Tayon <tayondw@gmail.com>
2025-05-21 11:59:31 -0400

    added user role to user model and changed types of created at, and updated at and added when the token expires for the user

--- auth-me/src/models/user.rs
+++ auth-me/src/models/user.rs
@@ -1,31 +1,84 @@
-use diesel::prelude::*;
+use std::io::Write;
+
+use diesel::{
+    prelude::*,
+    pg::{ Pg, PgValue },
+    serialize::{ self, Output, ToSql, IsNull },
+    deserialize::{ self, FromSql },
+    sql_types::SqlType,
+    AsExpression,
+    FromSqlRow,
+};
 use serde::{ Deserialize, Serialize };
+use chrono::{ DateTime, Utc };
+use uuid::Uuid;
+
 use crate::schema::users;
-use chrono;
+
+#[derive(SqlType)]
+#[diesel(postgres_type(name = "user_role"))]
+pub struct UserRoleType;
+
+#[derive(Debug, Deserialize, Serialize, Clone, Copy, AsExpression, PartialEq, FromSqlRow)]
+#[diesel(sql_type = UserRoleType)]
+pub enum UserRole {
+    Admin,
+    User,
+}
+
+impl UserRole {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            UserRole::Admin => "admin",
+            UserRole::User => "user",
+        }
+    }
+}
+
+// Serialize enum to SQL
+impl ToSql<UserRoleType, Pg> for UserRole {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        out.write_all(self.to_str().as_bytes())?;
+        Ok(IsNull::No)
+    }
+}
+
+// Deserialize enum from SQL
+impl FromSql<UserRoleType, Pg> for UserRole {
+    fn from_sql(value: PgValue<'_>) -> deserialize::Result<Self> {
+        match value.as_bytes() {
+            b"admin" => Ok(UserRole::Admin),
+            b"user" => Ok(UserRole::User),
+            _ => Err("Unrecognized enum variant for user_role".into()),
+        }
+    }
+}
 
 #[derive(Queryable, Serialize, Deserialize, Debug, Selectable, Identifiable, Clone)]
 #[diesel(table_name = users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
-    pub id: i32,
+    pub id: Uuid,
     pub name: String,
-    pub username: String,
     pub email: String,
+    pub username: String,
     pub password: String,
-    pub created_at: chrono::NaiveDateTime,
-    pub updated_at: chrono::DateTime<chrono::Utc>,
-    pub is_verified: bool,
+    pub verified: bool,
     pub verification_token: Option<String>,
+    pub token_expires_at: Option<DateTime<Utc>>,
+    pub role: UserRole,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
 }
 
 #[derive(Insertable, Deserialize, Debug)]
 #[diesel(table_name = users)]
 pub struct NewUser {
     pub name: String,
-    pub username: String,
     pub email: String,
+    pub username: String,
     pub password: String,
-    pub is_verified: bool,
+    pub verified: bool,
     pub verification_token: Option<String>,
 }
 
