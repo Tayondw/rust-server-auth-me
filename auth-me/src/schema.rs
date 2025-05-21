b6a5adacc5872f67de28142a656bacbff38ce1c1
Tayon <tayondw@gmail.com>
2025-05-21 10:54:58 -0400

    updated the user table to reflect how I want the project to go and removed posts

--- auth-me/src/schema.rs
+++ auth-me/src/schema.rs
@@ -1,38 +1,31 @@
 // @generated automatically by Diesel CLI.
 
-diesel::table! {
-    posts (id) {
-        id -> Int4,
-        #[max_length = 255]
-        title -> Varchar,
-        #[max_length = 255]
-        content -> Varchar,
-        created_at -> Timestamp,
-        updated_at -> Timestamp,
-        user_id -> Int4,
-    }
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "user_role"))]
+    pub struct UserRole;
 }
 
 diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::UserRole;
+
     users (id) {
-        id -> Int4,
-        #[max_length = 50]
+        id -> Uuid,
+        #[max_length = 100]
         name -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
         #[max_length = 50]
         username -> Varchar,
-        #[max_length = 50]
-        email -> Varchar,
-        password -> Text,
-        created_at -> Timestamp,
-        updated_at -> Timestamptz,
-        is_verified -> Bool,
+        #[max_length = 255]
+        password -> Varchar,
+        verified -> Bool,
+        #[max_length = 255]
         verification_token -> Nullable<Varchar>,
+        token_expires_at -> Nullable<Timestamptz>,
+        role -> UserRole,
+        created_at -> Nullable<Timestamptz>,
+        updated_at -> Nullable<Timestamptz>,
     }
 }
-
-diesel::joinable!(posts -> users (user_id));
-
-diesel::allow_tables_to_appear_in_same_query!(
-    posts,
-    users,
-);
