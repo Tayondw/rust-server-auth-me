10ffd67b002a44c91df616a53ca42a6c4b60068e
Tayon <tayondw@gmail.com>
2025-05-21 20:23:30 -0400

    added layer for role check on users route

--- auth-me/src/routes/api/users_router.rs
+++ auth-me/src/routes/api/users_router.rs
@@ -1,11 +1,14 @@
 use std::sync::Arc;
-use axum::{ routing::{ get, patch, post }, Router };
+
+use axum::{ routing::{ get, patch, post }, Router, middleware };
 use crate::{
     AppState,
     handlers::{
         user_handlers::*,
         authentication_handlers::{ signup_handler, verify_email_handler },
     },
+    middleware::auth::role_check,
+    models::UserRole
 };
 
 // USER ROUTER
@@ -14,6 +17,11 @@ pub fn user_routes(state: Arc<AppState>) -> Router<Arc<AppState>> {
         .route("/signup", post(signup_handler))
         .route("/api/auth/verify", get(verify_email_handler))
         .route("/api/users", get(get_users).post(create_user_handler))
+        .layer(
+            middleware::from_fn(|state, req, next| {
+                role_check(state, req, next, vec![UserRole::Admin])
+            })
+        )
         .route(
             "/api/users/{id}",
             patch(update_user_handler).get(get_user_by_id).delete(delete_user_handler)
