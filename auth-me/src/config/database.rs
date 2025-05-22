5992ddfc948feeef86dd3a00020a4331b22e95ec
Tayon <tayondw@gmail.com>
2025-05-21 20:02:29 -0400

    added verified token and add verified token functions, added with pool function for testing, updated config error enum to handle diesel errors, added validate function to rawdb config to handle any incorrect numbers or empty strings

--- auth-me/src/config/database.rs
+++ auth-me/src/config/database.rs
@@ -1,12 +1,39 @@
 use std::env;
+use std::time::Duration;
+
 use thiserror::Error;
 use diesel::{ prelude::*, PgConnection };
+use diesel::result::Error as DieselError;
 use diesel::r2d2::{ Pool, ConnectionManager, PoolError as R2D2Error };
+
 use serde::Deserialize;
+
+use chrono::{ NaiveDateTime, Utc };
+
+use uuid::Uuid;
 
 use crate::{ schema::users::dsl::*, models::User, dto::user_dtos::UserQuery };
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error("Environment variable not found: {0}")] MissingEnv(#[from] env::VarError),
+
+    #[error("Configuration error: {0}")] Config(String),
+
+    #[error("Database error: {0}")] Diesel(#[from] DieselError),
+
+    #[error("Connection pool error: {0}")] Pool(#[from] R2D2Error),
+
+    #[error("Unauthorized access")]
+    Unauthorized,
+
+    #[error("Not found")]
+    NotFound,
+
+    #[error("Internal server error")] Internal(String),
+}
 
 /*
 serde::Deserialize cannot be derived for types like Pool<ConnectionManager<PgConnection>>, since they do not implement Deserialize and cannot be deserialized directly from configuration files or JSON.
@@ -27,6 +54,37 @@ pub struct RawDatabaseConfig {
     pub jwt_refresh_expires_in: i64,
 }
 
+// Basic validation to check for empty strings or invalid numbers
+impl RawDatabaseConfig {
+    pub fn validate(&self) -> Result<(), ConfigError> {
+        if self.database_url.trim().is_empty() {
+            return Err(ConfigError::Config("DATABASE_URL cannot be empty".into()));
+        }
+        if self.jwt_secret.trim().is_empty() {
+            return Err(ConfigError::Config("JWT_SECRET cannot be empty".into()));
+        }
+        if self.jwt_refresh_secret.trim().is_empty() {
+            return Err(ConfigError::Config("JWT_REFRESH_SECRET cannot be empty".into()));
+        }
+        if self.rust_log.trim().is_empty() {
+            return Err(ConfigError::Config("RUST_LOG cannot be empty".into()));
+        }
+        if self.schema.trim().is_empty() {
+            return Err(ConfigError::Config("SCHEMA cannot be empty".into()));
+        }
+        if self.jwt_expires_in <= 0 {
+            return Err(ConfigError::Config("JWT_EXPIRES_IN must be greater than zero".into()));
+        }
+        if self.jwt_refresh_expires_in <= 0 {
+            return Err(
+                ConfigError::Config("JWT_REFRESH_EXPIRES_IN must be greater than zero".into())
+            );
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct DatabaseConfig {
     pub database_url: String,
@@ -39,18 +97,15 @@ pub struct DatabaseConfig {
     pub pool: PgPool,
 }
 
-#[derive(Error, Debug)]
-pub enum ConfigError {
-    #[error("Environment variable not found: {0}")] MissingEnv(#[from] env::VarError),
-
-    #[error("Configuration error: {0}")] Other(String),
-}
-
 impl DatabaseConfig {
     /// Build from a RawDatabaseConfig (which contains loaded fields).
-    pub fn from_raw(raw: RawDatabaseConfig) -> Result<Self, R2D2Error> {
+    pub fn from_raw(raw: RawDatabaseConfig) -> Result<Self, ConfigError> {
+        raw.validate()?;
         let manager = ConnectionManager::<PgConnection>::new(&raw.database_url);
-        let pool = Pool::builder().build(manager)?;
+        let pool = Pool::builder()
+            .max_size(15)
+            .connection_timeout(Duration::from_secs(5))
+            .build(manager)?;
 
         Ok(Self {
             pool,
@@ -76,44 +131,102 @@ impl DatabaseConfig {
                 ::var("JWT_EXPIRES_IN")?
                 .parse()
                 .map_err(|e| {
-                    ConfigError::Other(format!("Failed to parse JWT_EXPIRES_IN: {}", e))
+                    ConfigError::Config(format!("Failed to parse JWT_EXPIRES_IN: {}", e))
                 })?,
             jwt_refresh_expires_in: env
                 ::var("JWT_REFRESH_EXPIRES_IN")?
                 .parse()
                 .map_err(|e| {
-                    ConfigError::Other(format!("Failed to parse JWT_REFRESH_EXPIRES_IN: {}", e))
+                    ConfigError::Config(format!("Failed to parse JWT_REFRESH_EXPIRES_IN: {}", e))
                 })?,
         };
 
+        env_logger::Builder
+            ::from_env(env_logger::Env::default().default_filter_or(&raw.rust_log))
+            .init();
+
         DatabaseConfig::from_raw(raw).map_err(|e| {
-            ConfigError::Other(format!("Database pool creation failed: {}", e))
+            ConfigError::Config(format!("Database pool creation failed: {}", e))
         })
     }
 
-    pub fn get_user(&self, query: UserQuery) -> Result<Option<User>, diesel::result::Error> {
-        let mut conn = self.pool
-            .get()
-            .map_err(|e| {
-                diesel::result::Error::DatabaseError(
-                    diesel::result::DatabaseErrorKind::UnableToSendCommand,
-                    Box::new(e.to_string())
-                )
-            })?;
+    pub fn get_user(&self, query: UserQuery) -> Result<Option<User>, ConfigError> {
+        let mut conn = self.pool.get()?;
 
         let result = match query {
             UserQuery::Id(user_id) =>
-                users.filter(id.eq(user_id)).first::<User>(&mut conn).optional(),
+                users.filter(id.eq(user_id)).first::<User>(&mut conn).optional()?,
             UserQuery::Email(email_str) =>
-                users.filter(email.eq(email_str)).first::<User>(&mut conn).optional(),
+                users.filter(email.eq(email_str)).first::<User>(&mut conn).optional()?,
             UserQuery::Name(name_str) =>
-                users.filter(name.eq(name_str)).first::<User>(&mut conn).optional(),
+                users.filter(name.eq(name_str)).first::<User>(&mut conn).optional()?,
             UserQuery::Username(name_str) =>
-                users.filter(username.eq(name_str)).first::<User>(&mut conn).optional(),
+                users.filter(username.eq(name_str)).first::<User>(&mut conn).optional()?,
             UserQuery::Token(token_str) =>
-                users.filter(verification_token.eq(token_str)).first::<User>(&mut conn).optional(),
+                users.filter(verification_token.eq(token_str)).first::<User>(&mut conn).optional()?,
         };
 
-        result
+        Ok(result)
+    }
+
+    pub fn verified_token(&self, token_str: &str) -> Result<(), ConfigError> {
+        let mut conn = self.pool.get()?;
+
+        let now = Utc::now().naive_utc();
+
+        let target_user = users
+            .filter(verification_token.eq(Some(token_str.to_string())))
+            .filter(token_expires_at.gt(now))
+            .first::<User>(&mut conn)
+            .optional()?;
+
+        if let Some(user) = target_user {
+            diesel
+                ::update(users.filter(id.eq(user.id)))
+                .set((
+                    verified.eq(true),
+                    verification_token.eq::<Option<String>>(None),
+                    token_expires_at.eq::<Option<NaiveDateTime>>(None),
+                    updated_at.eq(now),
+                ))
+                .execute(&mut conn)?;
+            Ok(())
+        } else {
+            Err(ConfigError::NotFound)
+        }
+    }
+
+    pub fn add_verified_token(
+        &self,
+        user_id: Uuid,
+        token: String,
+        expires_at: NaiveDateTime
+    ) -> Result<(), ConfigError> {
+        let mut conn = self.pool.get()?;
+
+        diesel
+            ::update(users.filter(id.eq(user_id)))
+            .set((
+                verification_token.eq(Some(token)),
+                token_expires_at.eq(Some(expires_at)),
+                updated_at.eq(Utc::now().naive_utc()),
+            ))
+            .execute(&mut conn)?;
+
+        Ok(())
+    }
+
+    // FOR TESTING PURPOSES
+    pub fn with_pool(pool: PgPool) -> Self {
+        Self {
+            database_url: "".into(),
+            jwt_secret: "".into(),
+            jwt_refresh_secret: "".into(),
+            rust_log: "".into(),
+            schema: "".into(),
+            jwt_expires_in: 0,
+            jwt_refresh_expires_in: 0,
+            pool,
+        }
     }
 }
