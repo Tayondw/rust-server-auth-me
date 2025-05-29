c5cd155c070f07958463fc559b0ff1b54373e791
Tayon <tayondw@gmail.com>
2025-05-29 19:11:01 -0400

    created a new migration for the user to hold who the user was created by and if the password needed to be forced changed

--- auth-me/src/schema.rs
+++ auth-me/src/schema.rs
@@ -1,14 +1,14 @@
 // @generated automatically by Diesel CLI.
 
 pub mod sql_types {
-    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[derive(Clone, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "user_role"))]
     pub struct UserRole;
 }
 
 diesel::table! {
     use diesel::sql_types::*;
-    use crate::models::UserRoleType;
+    use super::sql_types::UserRole;
 
     users (id) {
         id -> Uuid,
@@ -24,7 +24,9 @@ diesel::table! {
         #[max_length = 255]
         verification_token -> Nullable<Varchar>,
         token_expires_at -> Nullable<Timestamptz>,
-        role -> UserRoleType,
+        role -> UserRole,
+        created_by -> Nullable<Uuid>,
+        force_password_change -> Bool,
         created_at -> Nullable<Timestamptz>,
         updated_at -> Nullable<Timestamptz>,
     }
