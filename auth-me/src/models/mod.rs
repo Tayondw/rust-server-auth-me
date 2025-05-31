fb166695f21dc05e7e13dea5283a18418aad1d1a
Tayon <tayondw@gmail.com>
2025-05-31 00:46:42 -0400

    created pending user model to stop the user from going into the database when signing up without verifying their email from the verify email

--- auth-me/src/models/mod.rs
+++ auth-me/src/models/mod.rs
@@ -1,7 +1,9 @@
 // import modules
 mod user;
 mod api;
+mod pending_user;
 
 // export modules
 pub use user::*;
 pub use api::*;
+pub use pending_user::*;
---
+++ auth-me/src/models/pending_user.rs
@@ -0,0 +1,37 @@
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+use serde::{ Deserialize, Serialize };
+use uuid::Uuid;
+
+use crate::{ schema::pending_users, models::UserRole };
+
+#[derive(Queryable, Serialize, Deserialize, Debug, Selectable, Identifiable, Clone)]
+#[diesel(table_name = pending_users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct PendingUser {
+    pub id: Uuid,
+    pub name: String,
+    pub email: String,
+    pub username: String,
+    pub password: String,
+    pub verification_token: String,
+    pub token_expires_at: NaiveDateTime,
+    pub role: UserRole,
+    pub created_by: Option<Uuid>,
+    pub force_password_change: bool,
+    pub created_at: Option<NaiveDateTime>,
+}
+
+#[derive(Insertable, Deserialize, Debug)]
+#[diesel(table_name = pending_users)]
+pub struct NewPendingUser {
+    pub name: String,
+    pub email: String,
+    pub username: String,
+    pub password: String,
+    pub verification_token: String,
+    pub token_expires_at: NaiveDateTime,
+    pub role: UserRole,
+    pub created_by: Option<Uuid>,
+    pub force_password_change: bool,
+}
